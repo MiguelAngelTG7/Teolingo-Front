066ae44f7fa74565b86ba78f512a409f35288d74
MiguelAngelTG7 <michael_angello@hotmail.com>
2025-08-21 01:54:51 -0500

    authservice change from email to username

--- src/api/authService.js
+++ src/api/authService.js
@@ -1,9 +1,9 @@
 // src/api/authService.js
 import api from './axiosConfig';
 
-export const login = async (email, password) => {
+export const login = async (username, password) => {
   const response = await api.post('token/', {
-    username: email,
+    username: username,
     password: password,
   });
   return response.data; // incluye access y refresh tokens
