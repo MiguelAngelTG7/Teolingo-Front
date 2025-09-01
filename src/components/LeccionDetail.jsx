cadf3ee4a3c2bc9f93bfed44997903c697ec11c5
MiguelAngelTG7 <michael_angello@hotmail.com>
2025-08-31 21:20:56 -0500

    progress try1

--- src/components/LeccionDetail.jsx
+++ src/components/LeccionDetail.jsx
@@ -63,7 +63,7 @@ export default function LeccionDetail() {
   const guardarProgreso = (resultado) => {
     const API_URL = import.meta.env.VITE_API_BASE_URL;
     
-    fetch(`${API_URL}/lecciones/${id}/respuesta/`, {  // Remove duplicate /api/
+    fetch(`${API_URL}/cursos/leccion/${id}/progreso/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
